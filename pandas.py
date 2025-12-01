1573702b45e269c334a9e97cb288e3b977775888
learnhive2025-crypto <learnhive2025@gmail.com>
2025-12-01 19:29:28 +0530

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,14 @@
+#1. What is Pandas? (Definition)
+#Pandas is a Python library used for
+#✔ Data analysis
+#✔ Data cleaning
+#✔ Data manipulation
+#✔ Working with tables like Excel/CSV
+#It provides two main data structures:
+#Series → 1-dimensional (like a column)
+#DataFrame → 2-dimensional table (rows & columns)
+#pip install pandas
+import pandas as pd
+s = pd.Series([10, 20, 30, 40])
+print(s)
+
